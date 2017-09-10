a3930c8c65ba8e33ba62fef88c3a4c8b0e8a0f6d
Lucy Laurentino <9998329@BCS.NET.NZ>
2017-09-11 11:34:00 +1200

    finish array

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace arraycsharp
 {
     class Program
@@ -9,8 +9,39 @@ namespace arraycsharp
             //Start the program with Clear();
             Console.Clear();
             
+           
+        //var fruits = new string[]{"Apple", "banana", "orange", "mandarin"};
 
-            Console.WriteLine("Hello World!");
+        var fruits = new List<string> {"Apple", "banana", "orange", "mandarin"};
+        var answer = true;
+        Console.WriteLine("here are a bunch of fruits");
+        Console.WriteLine();
+
+        do
+        {
+            foreach(var x in fruits)
+            Console.WriteLine(x);
+        }
+
+        Console.WriteLine("please add another fruit:");
+        var input = Console.ReadLine();
+
+        fruits.Add(input);
+        
+        Console.WriteLine("would you like to add another fruit?");
+        var inputAnswer = Console.ReadLine();
+        if (inputAnswer == "yes" )
+        //answer = bool.TryParse(Console.ReadLine(), out answer);
+        Console.WriteLine(answer);
+
+            
+        }
+        while(answer == true);
+        foreach(var)
+
+        
+
+           
             
             //End the program with blank line and instructions
             Console.ResetColor();
@@ -19,5 +50,6 @@ namespace arraycsharp
             Console.ReadKey();
             
         }
-    }
-}
+        }
+        }
+    
